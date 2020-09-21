45311329e50eee6407fd36d564b9b6b597123f28
Erina Delinicolas <erina.delinicolas@gmail.com>
2020-09-21 18:49:27 +1000

    added audio sources and play intro and background music on play

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+
+    public AudioSource intro;
+    public AudioSource background_music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background_music.PlayDelayed(1f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
